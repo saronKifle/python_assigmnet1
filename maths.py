a02269009dc99b0b371b5affed9145b6656d7b24
saron <saaronkifle786@gmail.com>
2025-01-29 23:02:04 +0300

    Added solution for math puzzle exercise

---
+++ maths.py
@@ -0,0 +1,14 @@
+base_number = 10 
+exponent = 3  
+
+power_result = base_number ** exponent
+
+large_number = 50
+quotient_result = large_number // base_number
+
+division_result = large_number / base_number
+
+final_value = power_result + quotient_result + division_result
+
+print( final_value)
+print( type(final_value))
